fb43563481b6b811912a21a4735cae65773ff121
vikas <vikaszende2004@gmail.com>
2026-05-22 23:14:04 +0530

    build

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import styles from "./Projects.module.css";
 
-import projects from "../../data/projects.json";
+import projects from "../../Data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
